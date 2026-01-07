a81ac19fd345d0d215f6c6a23c3ee8d59e755e67
Akash Kaushal <145990930+Akash-Kaushal@users.noreply.github.com>
2026-01-07 17:09:22 +0000

    today

--- first/1.2day.js
+++ first/1.2day.js
@@ -12,3 +12,26 @@ let user1 = {
 let user2= user1
 console.log(user2.userId)  
 //with another object we using same memory location because in same place in heap
+
+// string interpolation
+value= "akash"
+rollno= 016
+console.log(`my name is ${value} and roll no is ${rollno}`) 
+ // not a this'   //it  is `
+
+// string is a object
+const gamename= new String('akash')
+console.log(gamename[1])
+console.log(gamename.length)   // there is so many method in string // go to mdn 
+console.log(gamename.toUpperCase())
+
+// number is also object  
+const num= new Number(200)
+console.log(num)   
+console.log(num.toString())  //numer also have methods
+
+// math is also object
+console.log(Math.min(5,6,3,8))
+console.log(Math.random())     //always b/w 0  to 1
+console.log(Math.floor(Math.random()*10))
+
