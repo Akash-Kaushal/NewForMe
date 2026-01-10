8ec7a45ed0320036f1d5a1d6b56e29f0f1427b7d
Akash Kaushal <145990930+Akash-Kaushal@users.noreply.github.com>
2026-01-10 17:31:59 +0000

    today

--- first/1.8day.js
+++ first/1.8day.js
@@ -1,2 +1,33 @@
 //scope of let, const, var
 // scope in IDE is different and in console of browser is different  ****
+// learn about window object which very useful
+
+// this
+const user={
+    userName: "Akash",
+    price: 200,
+    welcomeMessage: function (){
+        console.log(`thankYou ${this.userName}, your total is ${this.price}`)
+        console.log(this)    // print many this
+    }}
+
+//user.welcomeMessage()
+user.userName="kaushalJi"
+user.welcomeMessage()
+
+function use(){
+    userName:"aaaa",
+    console.log(this.userName)   // this method is not work without object
+}
+use()
+
+
+//arrow function
+const newUser = () => {
+    userName= "akash",
+    age= 16,
+    console.log(userName)
+}
+newUser()
+
+
---
+++ first/1.9day.js
@@ -0,0 +1,7 @@
+// javascript execution context
+// global execution context     //on browser
+// function exection context
+// eval execution context
+// chai aur code javascript timestamp 5:56:00
+// also study high order array loop 7:51:00
+// also study filter and map  last part 
